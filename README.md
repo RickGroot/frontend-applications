522a451b89f44d866949eaa98b204435be6bdb14
Rick Groot <RickGroot@gmail.com>
2020-11-16 10:12:44 +0100

    clone from frontend-data repo

---
+++ README.md
@@ -0,0 +1,49 @@
+# Frontend Data
+A repository for Frontend Data, subject of the Information Design semester of CMD.    
+
+[*LINK TO SOURCES*](https://github.com/Rickert41/frontend-data/blob/main/sources.md)
+
+# Student information
+```javascript
+const name = "Rick Groot";
+const student_nmbr = "N/A";
+const school_mail = "rick.groot2@hva.nl";
+let age = 19;
+
+let ID_info = {
+        year: 2020,
+        semester: 1,
+        course: "Tech"
+};
+```
+
+# Research topic
+Are parking garages in The Netherlands high enough for people on vacation.
+
+## Sub-topics
+In which area's are more low parking garages?  
+Are there many parking garages outside cities?  
+
+## Required variables
+* ID of parking space
+* Location of parking garages
+* Capacity and other garage information, for better visualisation
+* Maximumheight of parking garage
+* Data of car types, like height and/or type    
+
+All this data is from RDW datasets. For location of parking garages I use [GEO Parkeer Garages](https://opendata.rdw.nl/Parkeren/GEO-Parkeer-Garages/t5pc-eb34), and for capacity and maximumvehicleheight I use [Open Data Parkeren: SPECIFICATIES PARKEERGEBIED](https://opendata.rdw.nl/Parkeren/Open-Data-Parkeren-SPECIFICATIES-PARKEERGEBIED/b3us-f26s). The datasets I am going to use for the car data are still unknown.
+
+## Expectations
+I think that lots of parking garages are perfectly compatible with most cars. But if you are on a vacation there could be a problem with some garages. I think that people with a roof box, or people who travel long distances in a travel van could be in trouble with some parking garages, because these vehicles are often higher.
+
+### Sub-topic expectations
+I think there are more low parking garages in older cities, because back then they might not have thought of taller vehicles. I also think there won't be many parking garages outside cities, because there is no need for them, and there is more space for regular parking lots.
+
+# Concept
+A map with all parking garages, with information about capacity and maximum vehicle heights. You can also filter the garages by filtering on your car height or car type, combined with a bar graph which contains the heights of parking garages, and the amount of these heights.
+Map of the Netherlands    |    Bar graph of most common heights
+:------------------------:|:------------------------|
+![](https://github.com/Rickert41/frontend-data/blob/main/utils/map.jpg)  |  ![](https://github.com/Rickert41/frontend-data/blob/main/utils/bar.jpg)
+
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
---
+++ script.js
@@ -0,0 +1,211 @@
+//import modules
+import {
+    endpoint,
+    endpoint2,
+    selectedColumn,
+} from './modules/endpoint.js';
+import {
+    compare
+} from './modules/array.js';
+import {
+    mapSVG,
+    mapProjection,
+    mapPath,
+    tooltip,
+    slider,
+    calculateRadius
+} from './modules/mapConst.js';
+import {
+    height,
+    x,
+    y,
+    barSVG,
+    barTooltip
+} from './modules/barConst.js';
+import {
+    getData,
+    filterData,
+    filterObjectValue
+} from './modules/filterFunctions.js';
+
+//data from endpoint 1
+let data1 = getData(endpoint) //calls function getData with API link
+    .then(result => { //only continues when data is fetched
+        return result.json()
+    })
+    .then(RDWData => {
+        const filteredDataObjects = filterObjectValue(RDWData, selectedColumn); //callsfilterObject with data and column ID
+
+        return filteredDataObjects;
+    })
+
+//data from endpoint 2
+let data2 = getData(endpoint2) //calls function getData with API link
+    .then(result => { //only continues when data is fetched
+        return result.json() //puts result into JSON
+    })
+    .then(RDWData => {
+        return RDWData;
+    })
+
+//all the clean data
+let cleanedDataObjects = compare(data1, data2) //calls compare function, and logs result when ready
+    .then(result => { //fires when data is collected
+        mapThings(result); //calls function that puts circles on the map
+        barchart(result); //calls function that places barchart
+        return result;
+    });
+
+//code for d3 map
+//load map data and put it in svg
+function loadMap() {
+    d3.json("https://gist.githubusercontent.com/larsbouwens/1afef9beb0c3df0e4b24/raw/5ed7eb4517eee5737a4cb4551558e769ed8da41a/nl.json").then(data => {
+
+        //draw the map
+        mapSVG.selectAll("#paths")
+            .selectAll("path")
+            .data(data.features)
+            .enter().append("path")
+            .attr("d", mapPath)
+    })
+
+    //zooming on map
+    mapSVG.call(d3.zoom()
+        .scaleExtent([1, 4]) //maximum zooming levels
+        .translateExtent([ //maximum panning levels
+            [-200, -200],
+            [1000, 800]
+        ])
+        .on('zoom', onZoom)); //calls function onZoom
+}
+
+window.onload = loadMap(); //loads the map after the page is loaded
+
+function mapThings(object) { //gets called when data is ready
+
+    //draw the circles on the map
+    mapSVG.select('#circles').selectAll('circle')
+        .data(object)
+        .enter().append('circle')
+        .attr('class', d => {
+            return d.maximumVehicleHeight
+        }) //adds class to interact with slider
+        .attr('cx', d => {
+            return mapProjection(d.location)[0];
+        }) //adds location from data object
+        .attr("cy", d => {
+            return mapProjection(d.location)[1];
+        })
+        .attr("r", d => {
+            return calculateRadius(d.capacity);
+        }) //calls function to calculate radius
+        .on("mouseover", mouseOverMap) //calls function when hovering
+        .on("mouseout", mouseOutMap) //calls function when not hovering
+}
+
+//when mouse is on circle
+function mouseOverMap(event, d) { //add interactivity
+
+    tooltip.html(d.areaDesc + '<br> Capacity: ' + d.capacity); //text of the tooltip
+
+    tooltip.style('left', (event.pageX) + 'px') //position of the tooltip
+        .style('top', (event.pageY + 10) + 'px')
+        .attr('class', 'focus'); //adds class for styling
+}
+
+//when mouse is not on circle
+function mouseOutMap() { //sets hover back when not hovering
+    tooltip.attr('class', 'unfocus'); //adds class to hide tooltip
+}
+
+//zoom with d3
+function onZoom(event) {
+    mapSVG.selectAll('svg g').attr('transform', event.transform);
+}
+
+
+//sets off an update when slider changes
+let value = slider.on('onchange', (val) => {
+    d3.select('#value').text(val); //chance html text
+    updateColor(val); //calls updatColor function
+});
+
+//function that updates circle color based on slider value
+function updateColor(val) {
+    document.querySelectorAll('circle').forEach(function colorFunc(d) { //chances classes with vanilla JS
+        if (parseInt(d.classList) > val) {
+            d.classList.remove('redDot'); //removes class if it's there
+            d.classList.add('greenDot'); //adds correct class
+        } else {
+            d.classList.remove('greenDot');
+            d.classList.add('redDot');
+        }
+    });
+}
+
+//code for bar chart
+//gets called when the data is collected
+function barchart(data) {
+
+    let heights = filterData(data, 'maximumVehicleHeight');
+    let barData = mergeValues(heights);
+
+    //scale the range of the data in the domains
+    x.domain(barData.map(function (d) {
+        return d.name; //sets names on x-axis
+    }).sort(d3.ascending));
+    y.domain([0, d3.max(barData, function (d) {
+        return d.amount; //sets max value of y-axis based on data
+    })]);
+
+    //append the bars for the bar chart
+    barSVG.selectAll(".bar")
+        .data(barData)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function (d) {
+            return x(d.name);
+        })
+        .attr("width", x.bandwidth())
+        .attr("y", function (d) {
+            return y(d.amount);
+        })
+        .attr("height", function (d) {
+            return height - y(d.amount);
+        })
+        .on("mouseover", mouseOverBar) //calls function when hovering
+        .on("mouseout", mouseOutBar) //calls function when not hovering
+
+    //add the x axis
+    barSVG.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    //add the y axis
+    barSVG.append("g")
+        .call(d3.axisLeft(y));
+}
+
+//puts all double values in a new object
+function mergeValues(data) {
+    return d3.groups(data, d => d)
+        .map(([name, value]) => ({ //creates array in object with name and value
+            name,
+            amount: d3.rollup(value, d => d).length
+        }))
+}
+
+//when mouse is on a bar
+function mouseOverBar(event, d) { //add interactivity
+
+    barTooltip.html(d.amount + ' parkeergarages met <br> een hoogte van ' + d.name + 'cm'); //text of the tooltip
+
+    barTooltip.style('left', (event.pageX) + 'px') //position of the tooltip
+        .style('top', (event.pageY + 10) + 'px')
+        .attr('class', 'focus'); //sets class that shows tooltip
+}
+
+//when mouse is off a bar
+function mouseOutBar() { //sets hover back when not hovering
+    barTooltip.attr('class', 'unfocus'); //sets class for not showing tooltip
+}
